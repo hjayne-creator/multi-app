e857b59fc4f9c53f19164af8b8babd9fd8832e73
Houston Jayne <hjayne2@yahoo.com>
2025-05-12 18:33:59 -0700

    celery worker

--- celery_worker.py
+++ celery_worker.py
@@ -1,4 +1,4 @@
-from apps.content_plan.celery_config import celery, flask_app
+from apps.content_plan.celery_config import celery
 
 # This file is used by Render to start the Celery worker
 # It imports and re-exports the Celery instance from your tasks module 
